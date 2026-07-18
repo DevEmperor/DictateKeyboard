be9f6c0f21dd9a2ab9b2564fb5e889418f071261
Alexander Immler <Alexander.Immler@immler.com>
2026-07-18 11:05:56 +0200

    Revert "experiment: use three threads for the speech gate"
    This reverts commit 19f2c14091d29636e3201471938fc585d0741515.

--- app/src/main/kotlin/dev/patrickgold/florisboard/dictate/audio/SpeechGate.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/dictate/audio/SpeechGate.kt
@@ -135,7 +135,7 @@ object SpeechGate {
                     maxSpeechDuration = 20f,
                 )
                 sampleRate = AudioDecode.TARGET_SAMPLE_RATE
-                numThreads = 3
+                numThreads = 2
             },
         )
     }.getOrNull()
